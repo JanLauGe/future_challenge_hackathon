9ac5245046d6623d14b4603668d1dc609bbd6cd8
JanLauGe <laurensgeffert@gmail.com>
2018-01-20 09:42:52 +0000

    initial commit

---
+++ code/EDA.R
@@ -0,0 +1,15 @@
+
+library(here)
+library(tidyverse)
+library(magrittr)
+
+here()
+
+df_city <- read_csv('data/CityData.csv')
+
+df_train <- read_csv('data/ForecastDataforTraining_201712.csv')
+df_holdout <- read_csv('data/ForecastDataforTesting_201712.csv')
+df_validation <- read_csv('data/In_situMeasurementforTraining_201712.csv')
+
+
+
